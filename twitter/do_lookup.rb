9f1c4aad80244ac59f355cd6d39ac8655d890ab3
booboy <bpmcdevitt@thelinuxspace.com>
2018-07-24 18:14:18 -0500

    made the filename reflect the class name and made a file for testing the lookup for now

---
+++ twitter/do_lookup.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+#
+load 'secret'
+require './twitter_follower_lookup.rb'
+
+username = 'bpmcdevitt'
+consumer_key = CONSUMER_KEY
+consumer_secret = CONSUMER_SECRET
+auth_token = ACCESS_TOKEN
+auth_secret = ACCESS_TOKEN_SECRET
+
+tw = TwitterFollowerLookup.new(username, consumer_key, consumer_secret, auth_token, auth_secret)
+
+follower_usernames = tw.follower_names
+
+follower_names.each do |username|
+  puts "#{username}.name"
+end
+
---
+++ twitter/twitter_follower_lookup.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+require 'twitter'
+require 'pry'
+
+class TwitterFollowerLookup
+  def initialize(username, consumer_key, consumer_secret, auth_token, auth_secret)
+    @username = username
+    @consumer_key = consumer_key
+    @consumer_secret = consumer_secret
+    @auth_token = auth_token
+    @auth_secret = auth_secret
+  end
+
+  def config_client
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key    = @consumer_key
+      config.consumer_secret = @consumer_secret
+      config.access_token = @access_token
+      config.access_token_secret = @access_token_secret
+    end
+  end
+
+  # handles the main api call, and also handles rate limiting, just let it sleep for 15 min its ok!
+  def get_follower_usernames
+    client = config_client
+    get_follower_usernames = client.followers(@username)
+    begin
+      get_follower_usernames.to_a
+    rescue Twitter::Error::TooManyRequests => error
+      # NOTE: Your process could go to sleep for up to 15 min but if you retry
+      # any sooner, it will almost certaintly fail with the same exception.
+      sleep error.rate_limit.reset_in + 1
+      retry
+    end
+  end
+
+  # dont think this one works, havent tested yet.
+  def follower_names
+    follower_names = get_follower_usernames.each(&:name)
+  end
+
+  def save_to_file(filename, data_to_write)
+    File.open(filename, 'w') do |file|
+      file.write(data_to_write)
+    end
+  end
+end
